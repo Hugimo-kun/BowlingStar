54a1fbf25215af7e666bda3678707557f9454a73
Hugimo-kun <hugo.simoneau@gmail.com>
2024-07-11 08:53:56 +0200

    70% of the page is done in html css, the calendar is here but need a little update, the maps part is done, the pictures just need a new animation to fix and the presentation is done

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  let calendarButtonToday = document.querySelector(
+    "fc-today-button fc-button fc-button-primary"
+  );
+
+  calendarButtonToday.value = "Aujourd'hui";
+});
